32915b6de6e801c874f53df39a9a314a6d5a707b
Macksuel Santos <macksuell_10@hotmail.com>
2014-09-22 15:26:45 -0300

    access not authorized

--- app/src/main/java/com/ocypode/volley/infrastructure/remote/handler/RequestHandler.java
+++ app/src/main/java/com/ocypode/volley/infrastructure/remote/handler/RequestHandler.java
@@ -1,12 +1,9 @@
 package com.ocypode.volley.infrastructure.remote.handler;
 
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.util.Log;
-import android.widget.Toast;
 
-import com.android.volley.AuthFailureError;
 import com.ocypode.volley.infrastructure.remote.volley.request.VolleyErrorHelper;
 
 public class RequestHandler<T> implements Handler<T> {
@@ -27,16 +24,6 @@ public class RequestHandler<T> implements Handler<T> {
 	@Override
 	public void onFail(Exception error) {
 		try {
-			if (error.getMessage() != null && error.getMessage().equals("java.io.IOException: No authentication challenges found")) {
-				accessNotAuthorized();
-				return;
-			} 
-			
-			if (error instanceof AuthFailureError) {
-				accessNotAuthorized();
-				return;
-			}
-				
 			String message = VolleyErrorHelper.getMessage(error, mContext);
 			AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
 			builder.setMessage(message);
@@ -45,13 +32,6 @@ public class RequestHandler<T> implements Handler<T> {
 		} catch(Exception e) {
 			Log.e("onFail", "RequestHandler.onFail", e);
 		}
-	}
-	
-	private void accessNotAuthorized() throws Exception {
-		Activity activity = ((Activity) mContext);
-		
-		Toast.makeText(activity, "You've been logged out by the server. Please login again.", Toast.LENGTH_LONG).show();
-		activity.onBackPressed();
 	}
 
 }
